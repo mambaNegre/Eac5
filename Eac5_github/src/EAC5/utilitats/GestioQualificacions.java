5ebcc3411ddc29a4ab6d4ae28ee5f3aba48bc2ae
moledov <moledov@mint.moledov>
2015-04-19 01:52:01 +0200

    Tercer prototipo de la clase GestioQualificacions.

--- Eac5_github/src/EAC5/utilitats/GestioQualificacions.java
+++ Eac5_github/src/EAC5/utilitats/GestioQualificacions.java
@@ -14,34 +14,35 @@ public class GestioQualificacions {
     
     /**
      * El mètode iniciarConcurs inicialitza la puntuació de tots els concursants
-     * a 0.
-     * @param puntsConcursants És l'array d'enters en què es desen les
-     * puntuacions de cada concursant.
-     * @param indexConcursants És l'array d'enters que mantindrà ordenats els
-     * concursants tant per NOM (primera columna) com per PUNTS (segona columna).
-     * @return L'array d'enters que manté els concursants ordenats.
+     * a 0 i crea l'índex de concursants.
+     * @param puntuacions Matriu d'enters de dues columnes; la primera columna
+     * conté la puntuació acumulada dels concursants, la segona columna manté
+     * l'ordre segons aquesta puntuació.
+     * @return Matriu d'enters amb la puntuació i índex dels concursants.
      */
-    public int[][] iniciarConcurs(int[][] puntsConcursants)  {
-        for (int i=0; i<puntsConcursants.length; i++) {
-            puntsConcursants[i][0] = 0;
-            puntsConcursants[i][1] = 1;
+    public int[][] iniciarConcurs(int[][] puntuacions)  {
+        // Es fa un recorreguts per tots els concursants
+        for (int i=0; i<puntuacions.length; i++) {
+            // Inicialitzar a 0 la puntuació de cada concursant
+            puntuacions[i][0] = 0;
+            // Per defecte, a cada concursant li correspon l'ordre en què va
+            // ser creat
+            puntuacions[i][1] = i;
         }
-        return puntsConcursants;
+        return puntuacions;
     }
     
-    /**
-     * El mètode ordenarPerPunts permet mantenir els concursants ordenats per
-     * la seva puntuació acumulada.
-     * @param puntsConcursants Array d'enters que emmagatzema la puntuació
-     * acumulada de cada concursant.
-     * @param indexConcursants Array d'enters amb dues columnes, la segona
-     * columna manté els concursants ordenats per PUNTS.
+    /** El mètode ordenarPerPunts permet mantenir els concursants ordenats
+     * per la seva puntuació acumulada.
+     * @param puntuacions Matriu d'enters de dues columnes; la primera columna
+     * conté la puntuació acumulada dels concursants, la segona columna manté
+     * l'ordre segons aquesta puntuació.
      * @param pos Enter que indica la posició a partir de la qual volem començar
      * a comprovar l'ordre.
      * @return La matriu de vectors que manté el concursants ordenats per NOM i
      * PUNTS.
      */
-    public int[] ordenarPerPunts(int[] puntsConcursants, int[] indexConcursants, int pos) {
+    public int[][] ordenarPerPunts(int[][] puntuacions, int pos) {
         // Per defecte es considera que els concursants no estan ordenats
         boolean ordenat = false;
         
@@ -53,30 +54,52 @@ public class GestioQualificacions {
             for (int i=pos; i>0; i--) {
                 // Fem servir dues variables auxiliars per carregar l'index de
                 // la posició actual i el que la precedeix
-                int posIndex1 = indexConcursants[i-1];
-                int posIndex2 = indexConcursants[i];
+                int posIndex1 = puntuacions[i-1][1];
+                int posIndex2 = puntuacions[i][1];
 
                 // Si la puntuació d'un concursant és major que la del següent
                 // es considera que el vector està ordenat
-                ordenat = puntsConcursants[posIndex1] >= puntsConcursants[posIndex2];
+                ordenat = puntuacions[posIndex1][0] >= puntuacions[posIndex2][0];
                 
                 if (!ordenat) {
                     // En cas que les puntuacions no estiguin ordenades,
                     // s'intercanvien els concursants
-                    indexConcursants[i-1] = posIndex2;
-                    indexConcursants[i] = posIndex1;
+                    puntuacions[i-1][1] = posIndex2;
+                    puntuacions[i][1] = posIndex1;
                 }
             }
         }
-        return indexConcursants;
+        return puntuacions;
     }
     
     /**
-     * 
-     * @param concursants
-     * @param puntsConcursants
-     * @param zonaEliminats
-     * @return 
+     * El mètode actualitzarIndex fa un recorregut per tots els concursants per
+     * mantenir l'ordre segons la puntuació acumulada.
+     * @param puntuacions Matriu d'enters de dues columnes; la primera columna
+     * conté la puntuació acumulada dels concursants, la segona columna manté
+     * l'ordre segons aquesta puntuació.
+     * @return La matriu de vectors que manté el concursants ordenats per NOM i
+     * PUNTS.
+     */
+    public int[][] actualitzarIndex(int[][] puntuacions) {
+        for (int i=1; i<puntuacions.length; i++) {
+            puntuacions = ordenarPerPunts(puntuacions, i);
+        }
+        return puntuacions;
+    }
+    
+    /**
+     * El mètode puntuarRonda demana a l'usuari que introudeix els punts que
+     * ha obtingut cada usuari a la ronda actual.
+     * @param concursants Matriu que emmagatzema les dades personals dels
+     * participants.
+     * @param puntuacions Matriu d'enters de dues columnes; la primera columna
+     * conté la puntuació acumulada dels concursants, la segona columna manté
+     * l'ordre segons aquesta puntuació.
+     * @param zonaEliminats Enter que indica el número de concursants eliminats
+     * en rondes prèvies.
+     * @return La matriu de vectors que manté el concursants ordenats per NOM i
+     * PUNTS.
      */
     public int[][] puntuarRonda(ArrayList<ArrayList<String>> concursants, int[][] puntuacions, int zonaEliminats) {
         int punts;
@@ -84,8 +107,7 @@ public class GestioQualificacions {
         //eliminats
         
         for (int i=0; i<concursants.size()-zonaEliminats; i++) {
-            System.out.println(concursants.size());
-            String str = "Introduex puntuació (0-3) per ";
+            String str = "Introdueix puntuació (0-3) per ";
             str += concursants.get(puntuacions[i][1]).get(NOM);
             str += " ";
             str += concursants.get(puntuacions[i][1]).get(COGNOMS);
@@ -97,13 +119,25 @@ public class GestioQualificacions {
         return puntuacions;
     }
     
+    /**
+     * El mètode llistarQualificacionsronda permet llistar els punts obtinguts
+     * per cada concursant a la ronda actual.
+     * @param concursants Matriu que emmagatzema les dades personals dels
+     * participants.
+     * @param puntuacions Matriu d'enters de dues columnes; la primera columna
+     * conté la puntuació acumulada dels concursants, la segona columna manté
+     * l'ordre segons aquesta puntuació.
+     * @param zonaEliminats Enter que indica el número de concursants eliminats
+     * en rondes prèvies.
+     */
     public void llistarQualificacionsRonda(ArrayList<ArrayList<String>> concursants, int[][] puntuacions, int zonaEliminats) {
         for (int i=0; i<puntuacions.length-zonaEliminats; i++) {
             String dni = UtilitatsString.formatCadena(concursants.get(i).get(DNI), 10, ' ', 0);
             String nom = UtilitatsString.formatCadena(concursants.get(i).get(NOM), 15, ' ', 0);
             String cognoms = UtilitatsString.formatCadena(concursants.get(i).get(COGNOMS), 20, ' ', 0);
             String telefon = UtilitatsString.formatCadena(concursants.get(i).get(DNI), 15, ' ', 0);
-            String punts = UtilitatsString.formatCadena(String.valueOf(puntuacions[i][1]), 5, ' ', -1);
+            int index = puntuacions[i][1];
+            String punts = UtilitatsString.formatCadena(String.valueOf(puntuacions[index][0]), 5, ' ', -1);
             System.out.println(dni + nom + cognoms + telefon + punts);
         }
     }
@@ -111,31 +145,48 @@ public class GestioQualificacions {
     /**
      * El mètode finalitzarRonda calcula els concursants eliminats a la ronda
      * actual del concurs.
-     * @param puntsConcursants Vector d'enters que conté la puntuació acumulada
-     * de cada condursant.
-     * @param indexConcursants Vector d'enters que maté els concursants ordenats
-     * per la seva puntuació.
-     * @param zonaEliminats Enter que indica a on comença la zona de concursants
-     * eliminats.
-     * @return Un enter que identifica on comença la zona d'eliminats després
-     * d'haver completat la ronda.
+     * @param puntuacions Matriu d'enters de dues columnes; la primera columna
+     * conté la puntuació acumulada dels concursants, la segona columna manté
+     * l'ordre segons aquesta puntuació.
+     * @param zonaEliminats Enter que indica el número de concursants eliminats
+     * en rondes prèvies.
+     * @return Un enter que actualitza el número de participants eliminasts
+     * al concurs.
      */
     public int finalitzarRonda(int[][] puntuacions, int zonaEliminats) {
         int limit = 0;
-        int max = puntuacions[limit][1];
+        int index = puntuacions[limit][1];
+        int max = puntuacions[index][0];
         
-        while (limit<zonaEliminats && puntuacions[limit][1]==max) {
+        while (zonaEliminats<puntuacions.length && puntuacions[index][0]==max) {
             limit++;
+            index = puntuacions[limit][1];
         }
         zonaEliminats = limit;
         return zonaEliminats;
     }
     
+    /**
+     * 
+     * @param concursants
+     * @param puntuacions
+     * @param zonaEliminats 
+     */
     public void llistarQualificacionsTwitter(ArrayList<ArrayList<String>> concursants, int[][] puntuacions, int zonaEliminats) {
-        for (int i=0; i<puntuacions.length-zonaEliminats; i++) {
+        System.out.println("CONCURSANTS ACTIUS");
+        for (int i=0; i<zonaEliminats; i++) {
             String nom = UtilitatsString.formatCadena(concursants.get(i).get(NOM), 15, ' ', 1);
             String cognoms = UtilitatsString.formatCadena(concursants.get(i).get(COGNOMS), 25, ' ', 1);
-            String punts = UtilitatsString.formatCadena(String.valueOf(puntuacions[i][1]), 5, ' ', -1);
+            int index = puntuacions[i][1];
+            String punts = UtilitatsString.formatCadena(String.valueOf(puntuacions[index][0]), 5, ' ', -1);
+            System.out.println(nom + cognoms + punts);
+        }
+        System.out.println("CONCURSANTS ELIMINATS");
+        for (int i=zonaEliminats; i<puntuacions.length; i++) {
+            String nom = UtilitatsString.formatCadena(concursants.get(i).get(NOM), 15, ' ', 1);
+            String cognoms = UtilitatsString.formatCadena(concursants.get(i).get(COGNOMS), 25, ' ', 1);
+            int index = puntuacions[i][1];
+            String punts = UtilitatsString.formatCadena(String.valueOf(puntuacions[index][0]), 5, ' ', -1);
             System.out.println(nom + cognoms + punts);
         }
     }
